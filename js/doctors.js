94fb964e425e3757e203498b740ce7a5a594c775
Cameron Jensen <jensenseanc@gmail.com>
2017-04-21 13:42:05 -0700

    add image banner and styling

--- js/doctors.js
+++ js/doctors.js
@@ -18,16 +18,17 @@ Doctor.prototype.getDoctor = function(medicalIssue) {
            'image_url' : result.data[i].profile.image_url,
            'address' : result.data[i].practices[0].visit_address.street,
            'zip' : result.data[i].practices[0].visit_address.zip,
-           'state' : result.data[i].practices[0].visit_address.state
+           'state' : result.data[i].practices[0].visit_address.state,
+           'phone' : result.data[i].practices[0].phones[0].number
          }
        });
      }
       namesArray.forEach(function(name){
-        console.log(name);
         $('#doctorResults').append(
-          "<div class='col-md-4'>" +
+          "<div class='col-md-3'>" +
           "<img src='" + name.doctor.image_url + "' alt='Doctors Pictures'>" +
           "<h3>" + name.doctor.first_name + " " + name.doctor.last_name + "</h3>" +
+          "<br>" + "<h4>Phone: </h4>" + name.doctor.phone +
           "<h4>Address: </h4>" +
           "<p>" + name.doctor.address + "<br>" + name.doctor.state + " " + name.doctor.zip + "</p>" +
           "</div>");
