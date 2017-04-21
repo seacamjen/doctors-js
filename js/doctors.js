2f796ad1fb404d9a2014522b2056c1eb8c55a3dc
Cameron Jensen <jensenseanc@gmail.com>
2017-04-21 12:03:05 -0700

    format how names will be in col

--- js/doctors.js
+++ js/doctors.js
@@ -9,13 +9,12 @@ Doctor.prototype.getDoctor = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
      for (i = 0; i < result.data.length; i ++) {
-       namesArray.push({'first_name' : result.data[i].profile.first_name});
+       namesArray.push({'first_name' : result.data[i].profile.first_name}, {'last_name' : result.data[i].profile.last_name}, {'image_url' : result.data[i].profile.image_url});
      }
-      console.log(result.data[0].profile.first_name);
-      console.log(result.data[1].profile.first_name);
-      console.log(namesArray);
-      console.log(namesArray[0].first_name);
-      $('#doctorResults').text(namesArray[0].first_name);
+      namesArray.forEach(function(name){
+        console.log(name);
+        $('#doctorResults').append("<div class='col-md-3'>" + name.first_name + name.last_name + name.image_url + "</div>");
+      })
     })
    .fail(function(error){
       console.log("fail");
