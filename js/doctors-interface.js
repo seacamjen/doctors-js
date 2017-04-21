1f169ff1a6f5cd53ab7da39273a0bc2dc2b89ec6
Cameron Jensen <jensenseanc@gmail.com>
2017-04-21 11:37:51 -0700

    add jquery to display information on index from doctors js

--- js/doctors-interface.js
+++ js/doctors-interface.js
@@ -4,15 +4,8 @@ var apiKey = require('./../.env').apiKey;
 $(document).ready(function () {
   $('#submitIssue').click(function() {
     var medicalIssue = $('#patientIssue').val();
-    console.log(medicalIssue);
 
     var searchDocs = new Doctor();
     var info = searchDocs.getDoctor(medicalIssue);
-
-    Promise.all([info]).then(function(values) {
-      $('#doctorResults').text("Here are your results " + info);
-    })
-
-
   });
 });
--- js/doctors.js
+++ js/doctors.js
@@ -4,19 +4,24 @@ function Doctor() {
 }
 
 var namesArray = [];
+
 Doctor.prototype.getDoctor = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
      for (i = 0; i < result.data.length; i ++) {
-       namesArray.push({'first_name' : result.data[i].profile.first_name}, {'last_name' : result.data[i].profile.last_name}, {'img' : result.data[i].profile.image_url});
+       namesArray.push({'first_name' : result.data[i].profile.first_name});
      }
-     console.log(namesArray);
       console.log(result.data[0].profile.first_name);
       console.log(result.data[1].profile.first_name);
+      console.log(namesArray);
+      console.log(namesArray[0].first_name);
+      $('#doctorResults').text(namesArray[0].first_name);
     })
    .fail(function(error){
       console.log("fail");
     });
+    return namesArray;
 };
+
 
 exports.doctorModule = Doctor;
