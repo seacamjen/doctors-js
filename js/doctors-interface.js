e5f12903d19182f96d378da9ce5d30012bb505c6
Cameron Jensen <cameron@>
2017-04-24 14:13:31 -0700

    move jquery from the backend into the interface

--- js/doctors-interface.js
+++ js/doctors-interface.js
@@ -1,6 +1,34 @@
 var Doctor = require('./../js/doctors.js').doctorModule;
 var apiKey = require('./../.env').apiKey;
 
+var displayByIssue = function(medicalIssue, namesArray) {
+  $('#doctorResults').empty();
+  namesArray.forEach(function(name){
+    $('#doctorResults').append(
+      "<div class='col-md-3'>" +
+      "<img src='" + name.doctor.image_url + "' alt='Doctors Pictures'>" +
+      "<h3>" + name.doctor.first_name + " " + name.doctor.last_name + "</h3>" +
+      "<br>" + "<h4>Phone: </h4>" + name.doctor.phone +
+      "<h4>Address: </h4>" +
+      "<p>" + name.doctor.address + "<br>" + name.doctor.state + " " + name.doctor.zip + "</p>" +
+      "</div>");
+    });
+  }
+
+var displayByDoctorName = function(medicalIssue, namesArray) {
+  $('#doctorResults').empty();
+  namesArray.forEach(function(name){
+    $('#doctorResults').append(
+      "<div class='col-md-3'>" +
+      "<img src='" + name.doctor.image_url + "' alt='Doctors Pictures'>" +
+      "<h3>" + name.doctor.first_name + " " + name.doctor.last_name + "</h3>" +
+      "<br>" + "<h4>Phone: </h4>" + name.doctor.phone +
+      "<h4>Address: </h4>" +
+      "<p>" + name.doctor.address + "<br>" + name.doctor.state + " " + name.doctor.zip + "</p>" +
+      "</div>");
+  });
+}
+
 $(document).ready(function () {
   $('#submitIssue').click(function() {
     var medicalIssue = $('#patientIssue').val();
@@ -8,15 +36,15 @@ $(document).ready(function () {
     $('#doctorInformation').hide();
 
     var searchDocs = new Doctor();
-    var info = searchDocs.getDoctor(medicalIssue);
-
+    searchDocs.getDoctor(medicalIssue, displayByIssue);
   });
+  
   $('#submitDocName').click(function() {
     var docName = $('#doctorName').val();
     $('#doctorName').val("");
     $('#doctorInformation').hide();
 
     var searchByDoc = new Doctor();
-    var info = searchByDoc.getDoctorName(docName);
+    searchByDoc.getDoctorName(docName, displayByDoctorName);
   });
 });
--- js/doctors.js
+++ js/doctors.js
@@ -3,17 +3,11 @@ var apiKey = require('./../.env').apiKey;
 function Doctor() {
 }
 
-
-Doctor.prototype.getDoctor = function(medicalIssue) {
+Doctor.prototype.getDoctor = function(medicalIssue, displayByIssue) {
   var namesArray = [];
-  $('#doctorResults').empty();
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-     if (result.data.length < 1) {
-       $('#doctorResults').append("<h2 class='center'>Search returned 0 results, please try another search</h2>")
-     }
      for (i = 0; i < result.data.length; i ++) {
-
        namesArray.push({
          "doctor" : {
            'first_name' : result.data[i].profile.first_name,
@@ -26,25 +20,16 @@ Doctor.prototype.getDoctor = function(medicalIssue) {
          }
        });
      }
-      namesArray.forEach(function(name){
-        $('#doctorResults').append(
-          "<div class='col-md-3'>" +
-          "<img src='" + name.doctor.image_url + "' alt='Doctors Pictures'>" +
-          "<h3>" + name.doctor.first_name + " " + name.doctor.last_name + "</h3>" +
-          "<br>" + "<h4>Phone: </h4>" + name.doctor.phone +
-          "<h4>Address: </h4>" +
-          "<p>" + name.doctor.address + "<br>" + name.doctor.state + " " + name.doctor.zip + "</p>" +
-          "</div>");
-      });
-    })
-   .fail(function(error){
-      console.log("fail");
+     console.log(medicalIssue);
+     console.log(namesArray);
+     displayByIssue(medicalIssue, namesArray);
+    }).fail(function(error){
+      $('#doctorResults').text(error.responseJSON.message);
     });
 };
 
-Doctor.prototype.getDoctorName = function(docName) {
+Doctor.prototype.getDoctorName = function(docName, displayByDoctorName) {
   var namesArray = [];
-  $('#doctorResults').empty();
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?name=' + docName + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
      for (i = 0; i < result.data.length; i ++) {
@@ -61,19 +46,10 @@ Doctor.prototype.getDoctorName = function(docName) {
          }
        });
      }
-      namesArray.forEach(function(name){
-        $('#doctorResults').append(
-          "<div class='col-md-3'>" +
-          "<img src='" + name.doctor.image_url + "' alt='Doctors Pictures'>" +
-          "<h3>" + name.doctor.first_name + " " + name.doctor.last_name + "</h3>" +
-          "<br>" + "<h4>Phone: </h4>" + name.doctor.phone +
-          "<h4>Address: </h4>" +
-          "<p>" + name.doctor.address + "<br>" + name.doctor.state + " " + name.doctor.zip + "</p>" +
-          "</div>");
-      });
+     displayByDoctorName(docName, namesArray);
     })
    .fail(function(error){
-      console.log("fail");
+      $('#doctorResults').text(error.responseJSON.message);
     });
 };
 
